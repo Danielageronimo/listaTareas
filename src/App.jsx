769437ee17e25aa818ccfc3ffddb55b923de0cf5
Daniela Geronimo <danielageronimo087@gmail.com>
2023-07-31 11:04:58 -0300

    agregue el boton borrar en mi items tarea

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import './App.css';
 import"bootstrap/dist/css/bootstrap.min.css";
 import FormularioTarea from "./components/FormularioTarea";
 
+function App() { 
 
-  function App() { 
   return (
     <>
-    <Container className="my-5 mainPage"
-    <h1 className='display-4 text-center text-light'>Lista de tareas</h1>
-    <hr className='text-light'/>
-    <FormularioTarea></FormularioTarea>
+    <Container className="my-5 mainPage">
+      <h1 className='display-4 text-center text-light'>Lista de tareas</h1>
+      <hr className='text-light'/>
+      <FormularioTarea></FormularioTarea>
     </Container>
     <footer className='text-center bg-dark text-light py-4'>
       <p>&copy; Todos los derechos reservados</p>
--- src/components/ItemsTarea.jsx
+++ src/components/ItemsTarea.jsx
@@ -1,11 +1,11 @@
-import { ListGroup, button } from "react-bootstrap";
+import { ListGroup, Button } from "react-bootstrap";
 
-const ItemsTarea = () {
+const ItemsTarea = () => {
   return (
       <ListGroup.Item className="d-flex justify-content-between">
-      ejemplo de tarea 
-      <button variant="danger">Borrar</Button>
-           </ListGroup.Item>
+        ejemplo de tarea 
+        <Button variant="danger">Borrar</Button>
+      </ListGroup.Item>
     );
   };
   
