ab67c20f18880ef9a99ff64b45f783b1f6a1088f
Daniela Geronimo <danielageronimo087@gmail.com>
2023-08-06 08:51:41 -0300

    agregamos las tareas en el array listaTareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -6,17 +6,24 @@ const FormularioTarea = () => {
  const [tarea, setTarea] =useState('');
  const [listaTareas, setListaTareas] =useState([]);
 
+  const handlenSubmin = (e) => {
+    e.preventDefault();
+    //guardar la tarea en el array listaTareas
+    setListaTareas([...listaTareas, tarea]);
+    //limpiar el value del input
+    setTarea('');
+  };
 
-  return (
+ return (
     <>
-    <Form>
+    <Form onSubmit={handlenSubmin}>
       <Form.Group className="mb-3 d-flex" controLId="tarea">
         <Form.Control type="text" 
         placeholder="ingrese una tarea" 
          value={tarea} 
          onChange={(e)=> setTarea(e.target.value) }
           />
-        <Button variant="primary">Agregar</Button>
+        <Button variant="primary" type="submit">Agregar</Button>
       </Form.Group>
     </Form>
     <ListaTareas></ListaTareas>
